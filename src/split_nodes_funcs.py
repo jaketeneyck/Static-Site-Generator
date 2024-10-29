5cb98e2f6b7bb942eb6714c5bf3afb5889fd0316
jaketeneyck <jaketeneyck3@gmail.com>
2024-10-29 12:08:15 -0400

    Added split_nodes_link function and testing suite

--- src/split_nodes_funcs.py
+++ src/split_nodes_funcs.py
@@ -48,4 +48,47 @@ def split_nodes_image(old_nodes):
     return new_nodes
 
 def split_nodes_link(old_nodes):
-    pass
+    new_nodes = []
+
+    for node in old_nodes:
+        link_props = extract_markdown_links(node.text)
+        sections = []
+
+        if len(link_props) == 0: # case for 0 images found
+            new_nodes.append(node)
+
+        elif len(link_props) == 1: # case for 1 image found
+            sections = node.text.split(f"[{link_props[0][0]}]({link_props[0][1]})", 1)
+            results = [
+                TextNode(sections[0], TextType.NORMAL),
+                TextNode(link_props[0][0], TextType.LINK, link_props[0][1]),
+                TextNode(sections[1], TextType.NORMAL)
+            ]
+            # Ensure no nodes with no text are added
+            for r in results:
+                if r.text != None and r.text != "":
+                    new_nodes.append(r)
+
+        else: # case for more than 1 image found
+            results = []
+            # Process the string before the first image tag and the tag itself
+            sections = node.text.split(f"[{link_props[0][0]}]({link_props[0][1]})", 1)
+            results.append(TextNode(sections[0], TextType.NORMAL))
+            results.append(TextNode(link_props[0][0], TextType.LINK, link_props[0][1]))
+
+            for i in range(1, len(link_props)):
+                # Remove the first string and run splits on the second string
+                sections.pop(0)
+                sections = sections[0].split(f"[{link_props[i][0]}]({link_props[i][1]})", 1)
+                results.append(TextNode(sections[0], TextType.NORMAL))
+                results.append(TextNode(link_props[i][0], TextType.LINK, link_props[i][1]))
+            
+            # Add the last chunk of text if the image node comes first
+            if len(sections) == 2:
+                results.append(TextNode(sections[1], TextType.NORMAL))
+
+            # Ensure no nodes with no text are added
+            for r in results:
+                if r.text != None and r.text != "":
+                    new_nodes.append(r)
+    return new_nodes
--- src/test_split_nodes_func.py
+++ src/test_split_nodes_func.py
@@ -9,18 +9,6 @@ class TestSplitNodesFuncs(unittest.TestCase):
     # ----
     # Testing split images function
     # ----
-    def test_split_nodes_images_two(self):
-        node = TextNode(
-            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
-            TextType.NORMAL,
-        )
-        expected = [
-            TextNode("This is text with a ", TextType.NORMAL),
-            TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
-            TextNode(" and ", TextType.NORMAL),
-            TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
-        ]
-        self.assertEqual(expected, split_nodes_image([node]))
 
     def test_split_nodes_images_none(self):
         node = TextNode(
@@ -39,6 +27,19 @@ class TestSplitNodesFuncs(unittest.TestCase):
         expected = [
             TextNode("This is text with a ", TextType.NORMAL),
             TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif")
+        ]
+        self.assertEqual(expected, split_nodes_image([node]))
+
+    def test_split_nodes_images_two(self):
+        node = TextNode(
+            "This is text with a ![rick roll](https://i.imgur.com/aKaOqIh.gif) and ![obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            TextType.NORMAL,
+        )
+        expected = [
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("rick roll", TextType.IMAGE, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("obi wan", TextType.IMAGE, "https://i.imgur.com/fJRm4Vk.jpeg"),
         ]
         self.assertEqual(expected, split_nodes_image([node]))
 
@@ -86,6 +87,85 @@ class TestSplitNodesFuncs(unittest.TestCase):
         ]
         self.assertEqual(expected, split_nodes_image(nodes))
 
-        
+    # ----
+    # Testing split links function
+    # ----
+
+    def test_split_nodes_links_none(self):
+        node = TextNode("This is a text node with no links", TextType.NORMAL)
+        expected = [
+            TextNode("This is a text node with no links", TextType.NORMAL)
+        ]
+        self.assertEqual(expected, split_nodes_link([node]))
+
+    def test_split_nodes_links_one(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev)",
+            TextType.NORMAL,
+        )
+        expected = [
+            TextNode("This is text with a link ", TextType.NORMAL),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+        ]
+        self.assertEqual(expected, split_nodes_link([node]))
+
+    def test_split_nodes_links_two(self):
+        node = TextNode(
+            "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)",
+            TextType.NORMAL,
+        )
+        expected = [
+            TextNode("This is text with a link ", TextType.NORMAL),
+            TextNode("to boot dev", TextType.LINK, "https://www.boot.dev"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("to youtube", TextType.LINK, "https://www.youtube.com/@bootdotdev"),
+        ]
+        self.assertEqual(expected, split_nodes_link([node]))
+
+    def test_split_nodes_links_first(self):
+        # Tests for when the image link is first in the list of nodes
+        node = TextNode(
+            "[This](https://i.imgur.com/aKaOqIh.gif) is a rick roll", TextType.NORMAL
+        )
+        expected = [
+            TextNode("This", TextType.LINK, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" is a rick roll", TextType.NORMAL)
+        ]
+        self.assertEqual(expected, split_nodes_link([node]))
+
+    def test_split_nodes_links_second(self):
+        # Tests for when there are two image links which come before the string nodes
+        node = TextNode(
+            "[This](https://i.imgur.com/aKaOqIh.gif) is a rick roll and [this](https://i.imgur.com/fJRm4Vk.jpeg) is obi wan", TextType.NORMAL
+        )
+        expected = [
+            TextNode("This", TextType.LINK, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" is a rick roll and ", TextType.NORMAL),
+            TextNode("this", TextType.LINK, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode(" is obi wan", TextType.NORMAL)
+        ]
+        self.assertEqual(expected, split_nodes_link([node]))
+
+    def test_split_nodes_links_multi(self):
+        # Tests for when multiple nodes are passed into the function
+        nodes = [
+            TextNode("This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            TextType.NORMAL,),
+            TextNode("This is text with a [rick roll](https://i.imgur.com/aKaOqIh.gif) and [obi wan](https://i.imgur.com/fJRm4Vk.jpeg)",
+            TextType.NORMAL,)
+        ]
+        expected = [
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("rick roll", TextType.LINK, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("obi wan", TextType.LINK, "https://i.imgur.com/fJRm4Vk.jpeg"),
+            TextNode("This is text with a ", TextType.NORMAL),
+            TextNode("rick roll", TextType.LINK, "https://i.imgur.com/aKaOqIh.gif"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("obi wan", TextType.LINK, "https://i.imgur.com/fJRm4Vk.jpeg"),
+        ]
+        self.assertEqual(expected, split_nodes_link(nodes))
+
+
 if __name__ == "__main__":
     unittest.main()
